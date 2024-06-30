051da29d7c0a32347ce06aa1cd2d4023af7bebec
juan <>
2024-06-30 16:40:50 -0300

    modificada para rangos de 1 año

--- R/getVarsBCRA.R
+++ R/getVarsBCRA.R
@@ -19,7 +19,7 @@ getPrincipalesVariables <- function() {
   return(result)
 }
 
-#' getDatosVariable
+#' _getDatosVariable
 #'
 #' @description Obtiene los datos de una variable del BCRA
 #'
@@ -28,11 +28,11 @@ getPrincipalesVariables <- function() {
 #' @param hasta fecha hasta la cual se quiere obtener los datos
 #'
 #' @return un tibble con los datos de la variable
-getDatosVariable <- function(idVariable, desde, hasta) {
+int_getDatosVariable = function(idVariable, desde, hasta) {
   require(httr2)
   require(tidyverse)
   require(jsonlite)
-  url <- paste0("https://api.bcra.gob.ar/estadisticas/v2.0/datosvariable/", idVariable, "/", desde, "/", hasta)
+  url = paste0("https://api.bcra.gob.ar/estadisticas/v2.0/datosvariable/", idVariable, "/", desde, "/", hasta)
 
   response = request(url) %>%
     req_headers(`Accept_Language` = "en-US") %>%
@@ -47,3 +47,45 @@ getDatosVariable <- function(idVariable, desde, hasta) {
   return(tibble(body))
 }
 
+#' getDatosVariable
+#'
+#' @description Wrapper que simplifica la periodicidad máxima que
+#' impuso el BCRA en los hits a la API (1 año). Llama a la función
+#' interna ("_") con su mismo nombre que es la que pega a la API.
+#'
+#' @param idVariable id de la variable a obtener
+#' @param desde fecha desde la cual se quiere obtener los datos
+#' @param hasta fecha hasta la cual se quiere obtener los datos
+#'
+#' @return un tibble con los datos de la variable
+getDatosVariable = function(idVariable, desde, hasta) {
+  # Convert the dates to Date class
+  desde <- as.Date(desde)
+  hasta <- as.Date(hasta)
+
+  # Initialize an empty list to store the results
+  all_data <- list()
+
+  # Loop through the date range and make API calls in 1-year intervals
+  current_start <- desde
+  while (current_start <= hasta) {
+    # Define the end date for the current interval
+    current_end = min(current_start + 364, hasta)  # 364 to ensure it doesn't exceed 1 year
+
+    # Make the API call
+    data <- bcra::int_getDatosVariable(idVariable = idVariable,
+                                   desde = as.character(current_start),
+                                   hasta = as.character(current_end))
+
+    # Store the result
+    all_data = append(all_data, list(data))
+
+    # Update the start date for the next interval
+    current_start = current_end + 1
+  }
+
+  # Combine all the results into a single data frame
+  combined_data = do.call(rbind, all_data)
+
+  return(combined_data)
+}
